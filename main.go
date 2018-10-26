f8a6164df8474d1cfe709babcb5f2e429ab093fd
Rex Beatie <rdev1163@outlook.com>
2018-10-25 20:45:17 -0700

    wip working on registration pattern still, connection works

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"ctl-auth/reporter"
+	"ctl-auth/wamp"
+	"log"
+	"os"
+)
 
+func main() {
+	logger := log.New(os.Stdout, "ctl-auth: ", 0)
+	rptChan := make(chan reporter.Report, 0)
+	mainReporter := reporter.Reporter{
+		Updater: rptChan,
+	}
+	conOpts := wamp.ConnectionOptions{
+		Url: "ws://127.0.0.1:3131",
+		Realm: "aura.registration",
+		Logger: logger,
+		Reporter: &mainReporter,
+	}
+	go wamp.ConnectService(conOpts)
+
+
+	for onNewReport := range rptChan {
+		logger.Println(onNewReport.ReportType().String())
+	}
 }
